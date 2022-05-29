94d5b7800fcf270e89e245f8a3b7b870b7bbd17b
Raven <joseph.smith2701@gmail.com>
2022-05-29 16:08:21 +0100

    lots of things added

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const generateImage = require("./generateImage");
+// const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
 
-client.on("ready", () => {
-  console.log(`logged in as ${client.user.tag}`);
-});
+let bot = {
+  client,
+  prefix: "n.",
+  owners: ["191394877047570433"],
+};
 
-client.on("messageCreate", (message) => {
-  if (message.content == "Hi") {
-    message.reply("Hello World");
-  }
-});
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+client.slashcommands = new Discord.Collection();
+client.buttons = new Discord.Collection();
 
-const welcomeChannelId = "980289754346176532";
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
+client.loadCommands = (bot, reload) =>
+  require("./handlers/commands")(bot, reload);
+client.loadSlashCommands = (bot, reload) =>
+  require("./handlers/slashcommands")(bot, reload);
+client.loadButtons = (bot, reload) =>
+  require("./handlers/buttons")(bot, reload);
 
-client.on("guildMemberAdd", async (member) => {
-  const img = await generateImage(member);
-  member.guild.channels.cache.get(welcomeChannelId).send({
-    content: `<@${member.id}>Welcome to the server `,
-    files: [img],
-  });
-});
+client.loadEvents(bot, false);
+client.loadCommands(bot, false);
+client.loadSlashCommands(bot, false);
+client.loadButtons(bot, false);
+
+module.exports = bot;
 
 client.login(process.env.TOKEN);
