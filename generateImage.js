7dff905a58a5f838a80dc7dcf64394c8d24c7081
Raven <joseph.smith2701@gmail.com>
2022-05-29 15:41:50 +0100

    added background picture and shape to the discord bot's welcome image

--- generateImage.js
+++ generateImage.js
@@ -1,6 +1,6 @@
 const Canvas = require("canvas");
 const Discord = require("discord.js");
-const background = "https://i.imugur.com/zvwWTUVu.jpg";
+const background = "https://i.imgur.com/zvWTUVu.jpg";
 
 const dim = {
   height: 675,
@@ -23,12 +23,14 @@ const generateImage = async (member) => {
     size: av.size,
   });
 
-  const canvas = Canvas.createCanvas(dim.Width, dim.height);
-  const ctx = Canvas.getContext("2d");
+  const canvas = Canvas.createCanvas(dim.width, dim.height);
+  const ctx = canvas.getContext("2d");
 
+  // draw in the background
   const backimg = await Canvas.loadImage(background);
   ctx.drawImage(backimg, 0, 0);
 
+  // draw black tinted box
   ctx.fillStyle = "rgba(0,0,0,0.8)";
   ctx.fillRect(
     dim.margin,
@@ -41,42 +43,45 @@ const generateImage = async (member) => {
   ctx.save();
 
   ctx.beginPath();
-  ctx.arc(av.x + av.size / 2, av.y + av.size / 2, 0, Math.PI * 2, true);
+  ctx.arc(
+    av.x + av.size / 2,
+    av.y + av.size / 2,
+    av.size / 2,
+    0,
+    Math.PI * 2,
+    true
+  );
   ctx.closePath();
   ctx.clip();
 
   ctx.drawImage(avimg, av.x, av.y);
   ctx.restore();
 
-  //write in text
-
+  // write in text
   ctx.fillStyle = "white";
   ctx.textAlign = "center";
 
-  //draw in welcome
-
+  // draw in Welcome
   ctx.font = "50px Roboto";
-  ctx.fillText = "Welcome,dimWidth/2 dim.marin + 70";
+  ctx.fillText("Welcome", dim.width / 2, dim.margin + 70);
 
-  //draw in the username
-
-  ctx.font - "60px Roboto";
+  // draw in the username
+  ctx.font = "60px Roboto";
   ctx.fillText(
     username + discrim,
     dim.width / 2,
     dim.height - dim.margin - 125
   );
 
-  //draw into the server
-
+  // draw in to the server
   ctx.font = "40px Roboto";
-  ctx.fillText("To the server", dim.width / 2, dim.height - 50);
+  ctx.fillText("to the server", dim.width / 2, dim.height - dim.margin - 50);
 
-  const attatchment = new Discord.MessageAttachment(
+  const attachment = new Discord.MessageAttachment(
     canvas.toBuffer(),
     "welcome.png"
   );
-  return attatchment;
+  return attachment;
 };
 
 module.exports = generateImage;
